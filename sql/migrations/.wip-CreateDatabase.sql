80b1969c79ea022ea853da6cb659e36aec79f002
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-11 01:06:26 -0500

    Update .wip-CreateDatabase.sql

--- sql/migrations/.wip-CreateDatabase.sql
+++ sql/migrations/.wip-CreateDatabase.sql
@@ -1,15 +1,28 @@
 create table users (
-  id uuid primary key default uuid_generate_v4()
+  id uuid primary key unique default uuid_generate_v4() not null
 );
 
-create table sellers (
-  id uuid default uuid_generate_v4(),
+create table oauth_claims (
+  id uuid unique default uuid_generate_v4() not null,
+  provider_uid text not null,
+  provider_id references oauth_providers(id) not null,
   user_id references users(id),
+  primary key (id, provider_uid, provider_id) 
+);
+
+create table oauth_providers (
+  id uuid unique default uuid_generate_v4() not null,
+  provider text unique not null,
+  primary key (id, provider)
+
+create table sellers (
+  id uuid unique default uuid_generate_v4() not null,
+  user_id unique references users(id) not null on delete restrict,
   primary key (id, user_id)
 );
 
 create table buyers (
-  id uuid default uuid_generate_v4(),
-  user_id references users(id) on delete restrict,
+  id uuid unique default uuid_generate_v4() not null,
+  user_id unique references users(id) not null on delete restrict,
   primary key (id, user_id)
-};
+);
