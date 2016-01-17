f26ae658a5cb207b48cfcfeb36e19513bfc86bb2
David J Felix <felix.davidj@gmail.com>
2016-01-17 01:37:35 +0000

    Fix the script and run it

--- sql/migrations/2016-01-17T00:57:26Z-CreateDatabase.sql
+++ sql/migrations/2016-01-17T00:57:26Z-CreateDatabase.sql
@@ -15,7 +15,7 @@ create table user_address(
   postal_code text not null
 );
 
-create table user_address_type (
+create table user_address_mqpings (
   id uuid primary key default uuid_generate_v4(),
   user_id uuid references users not null,
   address_id uuid references user_address not null,
@@ -23,27 +23,27 @@ create table user_address_type (
 );
 
 create table oauth_providers (
-  id uuid default uuid_generate_v4(),
+  id uuid unique default uuid_generate_v4() not null,
   provider text unique not null,
   primary key (id, provider)
 );
 
 create table oauth_claims (
-  id uuid default uuid_generate_v4(),
-  provider_uid text not null,
-  provider_id uuid references oauth_providers(id),
-  user_id uuid references users(id),
-  primary key (id, provider_uid, provider_id) 
+  id uuid unique default uuid_generate_v4() not null,
+  provider_uid text,
+  provider_id uuid references oauth_providers(id) not null,
+  user_id uuid references users(id) not null,
+  primary key (id, provider_uid)
 );
 
 create table sellers (
-  id uuid default uuid_generate_v4(),
-  user_id uuid references(id) on delete restrict,
+  id uuid unique default uuid_generate_v4(),
+  user_id uuid references users(id) on delete restrict,
   primary key (id, user_id)
 );
 
 create table buyers (
-  id uuid default uuid_generate_v4(),
+  id uuid unique default uuid_generate_v4(),
   user_id uuid references users(id) on delete restrict,
   primary key (id, user_id)
 );
