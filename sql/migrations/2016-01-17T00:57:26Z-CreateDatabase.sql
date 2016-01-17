fa2f891087a87818968d7811c994ad3ba8590261
David J. Felix <DavidJFelix@users.noreply.github.com>
2016-01-16 20:19:04 -0500

    Primary key implies unique and not null

--- sql/migrations/2016-01-17T00:57:26Z-CreateDatabase.sql
+++ sql/migrations/2016-01-17T00:57:26Z-CreateDatabase.sql
@@ -1,14 +1,14 @@
 create extension if not exists "uuid-ossp";
 
 create table users (
-  id uuid primary key unique default uuid_generate_v4() not null
+  id uuid primary key default uuid_generate_v4()
 );
 
 create table user_address(
-  id uuid primary key unique default uuid_generate_v4() not null,
+  id uuid primary key default uuid_generate_v4(),
   address_line_one text not null,
-  address_line_two text,
-  address_line_three text,
+  address_line_two text not null,
+  address_line_three text not null,
   city text not null,
   state_code text not null,
   country_code text not null,
@@ -16,35 +16,35 @@ create table user_address(
 );
 
 create table user_address_type (
-  id uuid primary key unique default uuid_generate_v4() not null,
-  user_id uuid references users(id) not null,
-  address_id uuid references user_address(id) not null,
+  id uuid primary key default uuid_generate_v4(),
+  user_id uuid references users not null,
+  address_id uuid references user_address not null,
   address_type text not null
 );
 
 create table oauth_providers (
-  id uuid unique default uuid_generate_v4() not null,
+  id uuid default uuid_generate_v4(),
   provider text unique not null,
   primary key (id, provider)
 );
 
 create table oauth_claims (
-  id uuid unique default uuid_generate_v4() not null,
+  id uuid default uuid_generate_v4(),
   provider_uid text not null,
-  provider_id uuid references oauth_providers(id) not null,
+  provider_id uuid references oauth_providers(id),
   user_id uuid references users(id),
   primary key (id, provider_uid, provider_id) 
 );
 
 create table sellers (
-  id uuid unique default uuid_generate_v4() not null,
-  user_id uuid unique references users not null on delete restrict,
+  id uuid default uuid_generate_v4(),
+  user_id uuid references(id) on delete restrict,
   primary key (id, user_id)
 );
 
 create table buyers (
-  id uuid unique default uuid_generate_v4() not null,
-  user_id uuid unique references users not null on delete restrict,
+  id uuid default uuid_generate_v4(),
+  user_id uuid references users(id) on delete restrict,
   primary key (id, user_id)
 );
 
